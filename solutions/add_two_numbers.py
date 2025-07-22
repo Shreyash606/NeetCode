78c125db6a7268f6cc954f27c1e32f3358b004dc
Shreyash Thakre <58138908+Shreyash606@users.noreply.github.com>
2025-07-22 00:45:37 -0500

    Add solution: Add Two Numbers

---
+++ solutions/add_two_numbers.py
@@ -0,0 +1,22 @@
+# Add Two Numbers
+
+**Language:** python  
+**Date:** July 22, 2025 at 12:45 AM  
+**Problem:** https://leetcode.com/problems/add-two-numbers/submissions/1706799222/
+
+## Solution
+
+```python
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy_head = ListNode(0)
+        curr = dummy_head
+```
+
+---
+*Automatically synced by LeetSync*
