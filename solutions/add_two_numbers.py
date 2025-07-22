c08f937226d91c8fb4d13de29735c49c5796171b
Shreyash Thakre <58138908+Shreyash606@users.noreply.github.com>
2025-07-22 10:57:06 -0500

    Add solution: Add Two Numbers

--- solutions/add_two_numbers.py
+++ solutions/add_two_numbers.py
@@ -1,8 +1,8 @@
 # Add Two Numbers
 
 **Language:** python  
-**Date:** July 22, 2025 at 10:08 AM  
-**Problem:** https://leetcode.com/problems/add-two-numbers/
+**Date:** July 22, 2025 at 10:57 AM  
+**Problem:** https://leetcode.com/problems/add-two-numbers/submissions/1707377928/
 
 ## Solution
 
@@ -16,6 +16,7 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         dummy_head = ListNode(0)
         curr = dummy_head
+        carry = 0
 ```
 
 ---
