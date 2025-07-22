2d05ad004f2c8bf3c8a3770ed7199ced55b11fd6
Shreyash Thakre <58138908+Shreyash606@users.noreply.github.com>
2025-07-22 12:20:27 -0500

    Add solution: 11. Container With Most Water

---
+++ solutions/11_container_with_most_water.py
@@ -0,0 +1,23 @@
+# 11. Container With Most Water
+
+**Language:** python  
+**Date:** July 22, 2025 at 12:20 PM  
+**Problem:** https://leetcode.com/problems/container-with-most-water/
+
+## Solution
+
+```python
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        res = 0
+        l = 0
+        r = len(height)-1
+        while(l<r):
+            h = min(height[l],height[r])
+            res = max(res,(h*(r-l)))
+            if height[l] < height[r]:
+                l+=1
+```
+
+---
+*Automatically synced by LeetSync*
