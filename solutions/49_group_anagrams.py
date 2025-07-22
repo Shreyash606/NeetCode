7eec1b9640ab0e1b6059c14ec73642aa7049590c
Shreyash Thakre <58138908+Shreyash606@users.noreply.github.com>
2025-07-22 12:45:52 -0500

    Add solution: 49. Group Anagrams

---
+++ solutions/49_group_anagrams.py
@@ -0,0 +1,15 @@
+# 49. Group Anagrams
+
+**Language:** python  
+**Date:** July 22, 2025 at 12:45 PM  
+**Problem:** https://leetcode.com/problems/group-anagrams/
+
+## Solution
+
+```python
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+```
+
+---
+*Automatically synced by LeetSync*
