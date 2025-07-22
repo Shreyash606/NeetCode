d692fab8bfc02a162cfa17a66352dc26d93fb81b
Shreyash Thakre <58138908+Shreyash606@users.noreply.github.com>
2025-07-22 18:54:15 -0500

    Update solution: 1. Two Sum

--- easy/1_two_sum.py
+++ easy/1_two_sum.py
@@ -1,6 +1,6 @@
 """
 Problem: 1. Two Sum
-URL: https://leetcode.com/problems/two-sum/description/
+URL: https://leetcode.com/problems/two-sum/submissions/1707844349/
 Difficulty: Easy
 Tags: None
 Date: 7/22/2025
@@ -8,14 +8,14 @@ Date: 7/22/2025
 LeetSync - Automated submission
 """
 
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hash_map = {}
-        
-        for i,num in enumerate(nums):
-            complement = target - num
-            if complement in hash_map:
-                return i,hash_map[complement]
-            else:
-                hash_map[num] = i
-        
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {}
+        
+        for i,num in enumerate(nums):
+            complement = target - num
+            if complement in hash_map:
+                return i,hash_map[complement]
+            else:
+                hash_map[num] = i
+        
