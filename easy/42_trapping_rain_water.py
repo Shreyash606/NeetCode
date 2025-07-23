8485e2f17242c0a9b62a0e68bbfac62c16cf7bf5
Shreyash Thakre <58138908+Shreyash606@users.noreply.github.com>
2025-07-22 19:26:37 -0500

    Add solution: 42. Trapping Rain Water

---
+++ easy/42_trapping_rain_water.py
@@ -0,0 +1,32 @@
+"""
+Problem: 42. Trapping Rain Water
+URL: https://leetcode.com/problems/trapping-rain-water/
+Difficulty: Easy
+Tags: None
+Date: 7/22/2025
+
+LeetSync - Automated submission
+"""
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        res = 0
+        l,r = 0, len(height) - 1
+        leftMax = height[l]
+        rightMax = height[r]
+
+        while(l<r):
+            if height[l] < height[r]:
+                l+=1
+                leftMax = max(leftMax,height[l])
+                res += leftMax - height[l]
+            else:
+                r-=1
+                rightMax = max(rightMax,height[r])
+                res += rightMax - height[r]
+        return res
+
+        # while(l<r):
+        #     if height[l] < height[r]:
+        #         l+=1
+        #         leftMax = max(leftMax,height[l])
